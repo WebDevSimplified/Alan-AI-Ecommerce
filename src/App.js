6e138cf89d50fa2e68d0fc81d98f075481143caf
Web Dev Simplified <webdevsimplified@gmail.com>
2020-11-24 11:35:17 -0600

    Starting Code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import storeItems from "./items.json"
 import Store from "./components/Store"
 import Cart from "./components/Cart"
-import useAlan from "./hooks/useAlan"
 
 function App() {
-  useAlan()
-
   return (
     <>
       <Store items={storeItems} />
